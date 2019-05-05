b42e7e94d5ae407264d3597ceb2449b37998541b
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2019-05-05 15:56:25 +0300

    Updated library

--- app/src/main/java/com/stoyanov5/funtoastlibrary/MainActivity.kt
+++ app/src/main/java/com/stoyanov5/funtoastlibrary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.stoyanov5.funtoastlibrary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.stoyanov5.toasterlibrary.FunToastMessage
 
 class MainActivity : AppCompatActivity() {
 
--- toasterlibrary/src/main/java/com/stoyanov5/toasterlibrary/FunToastMessage.kt
+++ toasterlibrary/src/main/java/com/stoyanov5/toasterlibrary/FunToastMessage.kt
@@ -3,10 +3,10 @@ package com.stoyanov5.toasterlibrary
 import android.content.Context
 import android.widget.Toast
 
-class FunToastMessage {
-    fun showFunToast(c: Context, message: String) {
+object FunToastMessage {
+
+    @JvmStatic fun showFunToast(c: Context, message: String) {
 
         Toast.makeText(c, "Woah a $message", Toast.LENGTH_SHORT).show()
-
     }
 }
